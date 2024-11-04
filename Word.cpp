5b8a3778cb2827e83375db5aa2eee14de3ce9a47
GavinKopf <164974314+GavinKopf@users.noreply.github.com>
2024-11-03 23:15:06 -0500

    Update Word.cpp

--- Word.cpp
+++ Word.cpp
@@ -1,11 +1,16 @@
-#include "Word.h"
+#include "word.h"
 #include<iostream>
 #include<string>
 #include<list>
-
-Word::Word(string s)
-{
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+Word::Word(string s){
     m_word = s;
+    m_currentWord.assign(m_word.length(), '_');
+}
+string Word::getFinalWord(){
+    return m_word;
 }
 string Word::getWord(){
     string w = "";
@@ -15,23 +20,33 @@ string Word::getWord(){
     }
     return w;
 }
+void Word::setWord(string s){
+    m_word = s;
+    m_currentWord.assign(s.length(), '_');
+}
 void Word::addLetter(char c){
     int i = 0;
-    for(list<char>::iterator it = m_currentWord.begin(); it != m_currentWord.end(); ++it){
-        if (c == m_word[i]){
+    for(auto it = m_currentWord.begin(); it != m_currentWord.end(); ++it){
+        if (c == toupper(m_word[i])){
             *it = c;
         }
         i++;
     }
 }
+void Word::randomWord(){
+    srand(time(0));
+    int i = rand()%110;
+    m_word = m_randomWords[i];
+    m_currentWord.assign(m_word.length(), '_');
+}
 bool Word::findLetter(char c){
     int i;
-    for (i = 0; i < m_word.length(); i++)
-        if(m_word[i] == c){
-            cout << "True!!!"<< endl;
+    tolower(c);
+    for (i = 0; i < m_word.length(); i++){
+        if(toupper(m_word[i]) == c){
             return true;
         }
-    cout << "False" << endl;
+    }
     return false;
 }
 bool Word::solved(){
